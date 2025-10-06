72f59218738c2d7c7a47f0b8f9a374fe6c48385c
JK <support@jk-powered.de>
2025-10-06 17:33:33 +0200

    Add saml-post-form loading indicator

--- src/login/pages/SamlPostForm.tsx
+++ src/login/pages/SamlPostForm.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../KcContext";
 import type { I18n } from "../i18n";
+import Button from "@mui/material/Button";
+import LinearProgress from '@mui/material/LinearProgress';
 
 export default function SamlPostForm(props: PageProps<Extract<KcContext, { pageId: "saml-post-form.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
@@ -27,6 +29,7 @@ export default function SamlPostForm(props: PageProps<Extract<KcContext, { pageI
     }, [htmlFormElement]);
     return (
         <Template kcContext={kcContext} i18n={i18n} doUseDefaultCss={doUseDefaultCss} classes={classes} headerNode={msg("saml.post-form.title")}>
+            <LinearProgress />
             <p>{msg("saml.post-form.message")}</p>
             <form name="saml-post-binding" method="post" action={samlPost.url} ref={setHtmlFormElement}>
                 {samlPost.SAMLRequest && <input type="hidden" name="SAMLRequest" value={samlPost.SAMLRequest} />}
@@ -34,7 +37,9 @@ export default function SamlPostForm(props: PageProps<Extract<KcContext, { pageI
                 {samlPost.relayState && <input type="hidden" name="RelayState" value={samlPost.relayState} />}
                 <noscript>
                     <p>{msg("saml.post-form.js-disabled")}</p>
-                    <input type="submit" value={msgStr("doContinue")} />
+                    <Button variant="contained"
+                            type="submit"
+                    >{msgStr("doContinue")}</Button>
                 </noscript>
             </form>
         </Template>
