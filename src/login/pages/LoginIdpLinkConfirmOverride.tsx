bd176bd7c31838a15c80803745feb691d5eeb071
JK <support@jk-powered.de>
2025-10-06 15:26:51 +0200

    Add login idp link confirm override restart button

--- src/login/pages/LoginIdpLinkConfirmOverride.tsx
+++ src/login/pages/LoginIdpLinkConfirmOverride.tsx
@@ -2,6 +2,7 @@ import { getKcClsx } from "keycloakify/login/lib/kcClsx";
 import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../KcContext";
 import type { I18n } from "../i18n";
+import Button from "@mui/material/Button";
 
 export default function LoginIdpLinkConfirmOverride(props: PageProps<Extract<KcContext, { pageId: "login-idp-link-confirm-override.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
@@ -19,20 +20,23 @@ export default function LoginIdpLinkConfirmOverride(props: PageProps<Extract<KcC
         <Template kcContext={kcContext} i18n={i18n} doUseDefaultCss={doUseDefaultCss} classes={classes} headerNode={msg("confirmOverrideIdpTitle")}>
             <form id="kc-register-form" action={url.loginAction} method="post">
                 {msg("pageExpiredMsg1")}{" "}
-                <a id="loginRestartLink" href={url.loginRestartFlowUrl}>
-                    {msg("doClickHere")}
-                </a>
-                <br />
-                <br />
-                <button
-                    type="submit"
-                    className={kcClsx("kcButtonClass", "kcButtonDefaultClass", "kcButtonBlockClass", "kcButtonLargeClass")}
-                    name="submitAction"
-                    id="confirmOverride"
-                    value="confirmOverride"
+                <Button
+                    id="loginRestartLink"
+                    variant="contained"
+                    href={url.loginRestartFlowUrl}
                 >
+                    {msg("doClickHere")}
+                </Button>
+                <br />
+                <br />
+                <Button variant="contained"
+                        type="submit"
+                        className={kcClsx("kcButtonClass", "kcButtonDefaultClass", "kcButtonBlockClass", "kcButtonLargeClass")}
+                        name="submitAction"
+                        id="confirmOverride"
+                        value="confirmOverride">
                     {msg("confirmOverrideIdpContinue", idpDisplayName)}
-                </button>
+                </Button>
             </form>
         </Template>
     );
