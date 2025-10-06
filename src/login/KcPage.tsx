1af3119819de0cf2a77462a1dd4ab234762c2244
JK <support@jk-powered.de>
2025-10-06 17:44:17 +0200

    Add login-x509-info page

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -17,6 +17,7 @@ const LoginPasskeysConditionalAuthenticate = lazy(() => import("./pages/LoginPas
 const Terms = lazy(() => import("./pages/Terms"));
 const Error = lazy(() => import("./pages/Error"));
 const Code = lazy(() => import("./pages/Code"));
+const LoginX509Info = lazy(() => import("./pages/LoginX509Info"));
 
 const doMakeUserConfirmPassword = true;
 
@@ -106,6 +107,14 @@ export default function KcPage(props: { kcContext: KcContext }) {
                     case "delete-account-confirm.ftl":
                         return (
                             <DeleteAccountConfirm
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+                    case "login-x509-info.ftl":
+                        return (
+                            <LoginX509Info
                                 {...{ kcContext, i18n, classes }}
                                 Template={Template}
                                 doUseDefaultCss={false}
