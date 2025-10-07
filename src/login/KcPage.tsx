3db3a986b65b41e4d7d72cb8e65a7e0c9e781f38
JK <support@jk-powered.de>
2025-10-07 10:37:04 +0200

    Add login-update-profile page

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -17,6 +17,7 @@ const LoginPasskeysConditionalAuthenticate = lazy(() => import("./pages/LoginPas
 const Terms = lazy(() => import("./pages/Terms"));
 const Error = lazy(() => import("./pages/Error"));
 const Code = lazy(() => import("./pages/Code"));
+const LoginUpdateProfile = lazy(() => import("./pages/LoginUpdateProfile"));
 
 const doMakeUserConfirmPassword = true;
 
@@ -109,6 +110,16 @@ export default function KcPage(props: { kcContext: KcContext }) {
                                 {...{ kcContext, i18n, classes }}
                                 Template={Template}
                                 doUseDefaultCss={false}
+                            />
+                        );
+                    case "login-update-profile.ftl":
+                        return (
+                            <LoginUpdateProfile
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                                UserProfileFormFields={UserProfileFormFields}
+                                doMakeUserConfirmPassword={doMakeUserConfirmPassword}
                             />
                         );
 
