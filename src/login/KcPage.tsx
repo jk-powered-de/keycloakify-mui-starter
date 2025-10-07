2cb8e7660d496543c5dce360048b1b346188ba24
JK <support@jk-powered.de>
2025-10-07 16:25:59 +0200

    Add login-oauth-grant page

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -17,6 +17,7 @@ const LoginPasskeysConditionalAuthenticate = lazy(() => import("./pages/LoginPas
 const Terms = lazy(() => import("./pages/Terms"));
 const Error = lazy(() => import("./pages/Error"));
 const Code = lazy(() => import("./pages/Code"));
+const LoginOauthGrant = lazy(() => import("./pages/LoginOauthGrant"));
 
 const doMakeUserConfirmPassword = true;
 
@@ -106,6 +107,14 @@ export default function KcPage(props: { kcContext: KcContext }) {
                     case "delete-account-confirm.ftl":
                         return (
                             <DeleteAccountConfirm
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+                    case "login-oauth-grant.ftl":
+                        return (
+                            <LoginOauthGrant
                                 {...{ kcContext, i18n, classes }}
                                 Template={Template}
                                 doUseDefaultCss={false}
